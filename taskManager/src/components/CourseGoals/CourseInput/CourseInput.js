844b3841e9997db22a694b9bf1c05d7b21bc6abf
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-12-24 13:31:17 +0000

    error notification styles added

--- taskManager/src/components/CourseGoals/CourseInput/CourseInput.js
+++ taskManager/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,29 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isvalid, setIsValid] = useState(false)
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0){
+      setIsValid(false)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0){
+      setIsValid(true)
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: isvalid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{backgroundColor: isvalid ? 'salmon' : 'transparent', borderColor: isvalid ? 'red' : '#ccc' }} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
